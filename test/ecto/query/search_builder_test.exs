02548d56dbad782641e40a917a148554fec29371
Moosieus <cameron.duley99@gmail.com>
2024-09-09 16:00:37 -0400

    start scaffolding tests

--- test/ecto/query/search_builder_test.exs
+++ test/ecto/query/search_builder_test.exs
@@ -3,8 +3,58 @@ Code.require_file "../../../integration_test/support/types.exs", __DIR__
 defmodule Ecto.Query.SearchBuilderTest do
   use ExUnit.Case, async: true
 
-  import Ecto.Query.Builder
-  doctest Ecto.Query.Builder
+  import Ecto.Query.SearchBuilder
+  doctest Ecto.Query.SearchBuilder
 
-  # SEARCH_TODO: test the search query builder functions
+  # SEARCH_TODO: test all the queries
+
+  describe "all/1" do
+    test "requires a bind as the first argument" do
+
+    end
+  end
+
+  describe "boost/2" do
+
+  end
+
+  describe "const_score/2" do
+
+  end
+
+  describe "disjunction_max/1" do
+
+  end
+
+  describe "disjunction_max/2" do
+
+  end
+
+  describe "empty/1" do
+
+  end
+
+  describe "exists/1" do
+
+  end
+
+  describe "fuzzy_term/2" do
+
+  end
+
+  describe "fuzzy_term/3" do
+
+  end
+
+  describe "more_like_this/n" do
+
+  end
+
+  describe "phrase/3" do
+
+  end
+
+  describe "parse/2" do
+
+  end
 end
